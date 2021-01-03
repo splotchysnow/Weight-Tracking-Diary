48480895e5d0d4b36cc7f7d37102c2b22fa32fdc
splotchysnow <46405838+splotchysnow@users.noreply.github.com>
2021-01-03 08:44:24 -0800

    updated Panels

--- src/Guan/MyFrame.java
+++ src/Guan/MyFrame.java
@@ -2,24 +2,37 @@ package Guan;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class MyFrame {
 
     public static void main(String[] args) {
         // Jframe is a GUI window to add components to:
+        final int childDefaultWidth = 200;
+        final int childDefaultHeight = 100;
+        final int grid = 10; //leaving 10 pixel on the grid
         JFrame frame = new JFrame();
 
         //Setting up the setting for the Jframe
         frame.setTitle("Weight Tracking Device"); // Set the frame title
-        frame.setSize(420,420); // set the x and y dimension of the frame.
+        frame.setSize(500,500); // set the x and y dimension of the frame.
         frame.setResizable(false); // prevent user from resizing the frame.
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
-        frame.setLayout(null);
 
         // Setting up background and Logo for the frame
         ImageIcon image = new ImageIcon("logo.PNG"); //create an ImageIcon
         frame.setIconImage(image.getImage()); // Change icon of frame
         frame.getContentPane().setBackground(new Color(200,220,255)); //Change color of the background
+
+        // Layout Manager = Defines the natural layout for components within a container
+        // FlowLayout = places components in a row, sized at their preferred size. If the horizontal space in the container is too small
+        // It will use the next available row.
+
+        //Use JPanel
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(200,200));
+        panel.setBackground(new Color(250,150,150));
+        panel.setLayout(new FlowLayout());
 
         //Setting up the title Image and title Label
         ImageIcon titleImage = new ImageIcon("titleImage.png"); //getting the icon for the titleLabel
@@ -27,15 +40,25 @@ public class MyFrame {
         Image newImg = titleImg.getScaledInstance(40,40, Image.SCALE_SMOOTH); //Transform the image smoothly
         titleImage = new ImageIcon(newImg);
 
+        //Set up titleLabel
         JLabel titleLabel = new JLabel("Weight Tracking: ", titleImage, 0); // set the text of the label and create the label
         titleLabel.setVerticalAlignment(0); //set the vertical alignment to 0
         titleLabel.setFont(new Font("MV Boli", Font.BOLD, 16)); // Set font to MV BOLI, BOLD it, and set the size to 16
-        titleLabel.setBounds(0,0,200,100); // Set the size 200*100 and location as (0,0)
-        frame.add(titleLabel);
-//        frame.pack(); //Automatically adjust the size of all component in the GUI automatically
+//        titleLabel.setBounds(grid,grid,childDefaultWidth,childDefaultHeight); // Set the size 200*100 and location as (0,0)
 
-        // Run the frame
-        frame.setVisible(true); // Set the frame visible
+        panel.add(titleLabel); //TODO: add this to a Boarder Layout instead of a panel layout.
+
+        //Text box and label for the date: coordinate something (0,1) and (0,2)
+        JLabel dateLabel = new JLabel("Date: ");         //set dateLabel
+        JTextField dateTextField = new JTextField("MM/DD/YYYY"); //set dateTextField
+        panel.add(dateLabel);
+        panel.add(dateTextField);
+
+
+        // Add everything and show the frame.
+        frame.add(panel);
+        frame.setVisible(true);
+
     }
 
 }
